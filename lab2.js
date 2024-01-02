7bf45dd605ea31c519cdd4b194d3356e6ef47a44
Ahmed Kamal <oudaa898@gmail.com>
2024-01-02 16:04:23 +0200

    lab2.

--- lab2.js
+++ lab2.js
@@ -1,62 +1,62 @@
 // Q1:
 
-// var flag = true
-// while (flag) {
-//     var age;
-//     var prop;
-//     var ageString = prompt("Please enter your age or type 'cancel' to exit:");
-//     if (ageString === "cancel") {
-//         alert("Exiting!");
-//         break;
-//     }
+var flag = true
+while (flag) {
+    var age;
+    var prop;
+    var ageString = prompt("Please enter your age or type 'cancel' to exit:");
+    if (ageString === "cancel") {
+        alert("Exiting!");
+        break;
+    }
 
-//     age = Number(ageString);
-//     if (age < 0) {
-//         alert("Please enter a positive number!!");
-//         continue;
-//     }
-//     switch (true) {
-//         case age >= 1 && age <= 10:
-//             prop = "You are a child.";
-//             break;
-//         case age >= 11 && age <= 18:
-//             prop = "You are a teenager.";
-//             break;
-//         case age >= 19 && age <= 50:
-//             prop = "You are a grown-up.";
-//             break;
-//         default:
-//             prop = "You are old.";
-//             break;
-//     }
+    age = Number(ageString);
+    if (age < 0) {
+        alert("Please enter a positive number!!");
+        continue;
+    }
+    switch (true) {
+        case age >= 1 && age <= 10:
+            prop = "You are a child.";
+            break;
+        case age >= 11 && age <= 18:
+            prop = "You are a teenager.";
+            break;
+        case age >= 19 && age <= 50:
+            prop = "You are a grown-up.";
+            break;
+        default:
+            prop = "You are old.";
+            break;
+    }
 
-//     alert(prop);
-// }
+    alert(prop);
+}
 //----------------------------------------------------------------------------------------------------------------
 // Q2:
 
-// string = prompt("Please Enter A String: ")
-// var arr = string.split('')
-// var i;
-// var counter = 0;
-// for (i of arr) {
-//     if (i === 'a' || i === 'A') {
-//         counter++;
-//     }
-//     if (i === 'o' || i === 'O') {
-//         counter++;
-//     }
-//     if (i === 'i' || i === 'I') {
-//         counter++;
-//     }
-//     if (i === 'e' || i === 'E') {
-//         counter++;
-//     }
-//     if (i === 'u' || i === 'U') {
-//         counter++;
-//     }
-// }
-// alert(`Number Of Vowls Is: ${counter}`)
+string = prompt("Please Enter A String: ")
+var arr = string.split('')
+var i;
+var counter = 0;
+for (i of arr) {
+    if (i === 'a' || i === 'A') {
+        counter++;
+    }
+    if (i === 'o' || i === 'O') {
+        counter++;
+    }
+    if (i === 'i' || i === 'I') {
+        counter++;
+    }
+    if (i === 'e' || i === 'E') {
+        counter++;
+    }
+    if (i === 'u' || i === 'U') {
+        counter++;
+    }
+}
+alert(`Number Of Vowls Is: ${counter}`)
 
 //----------------------------------------------------------------------------------------------------------------
 // Q3:
